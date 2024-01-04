19e0d107ae54150c17f486e9cbbbc030832c91fb
bion-slmn <bionsol25@gmail.com>
2024-01-04 23:51:19 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,37 @@
+'''this module answers a pascal triangle question'''
+
+
+def factorial(n):
+    '''this definesa factorial solution for a given number'''
+    if n == 0:
+        return 1
+
+    return n * factorial(n-1)
+
+
+def comb(x, y):
+    '''defines a combination formulat'''
+    a = factorial(x)
+    b = factorial(y)
+    c = factorial(x - y)
+
+    return a // (b * c)
+
+
+def pascal_triangle(n):
+    ''' returns a list of lists of integers representing
+    the Pascal’s triangle of n'''
+    matrix = []
+
+    if n <= 0:
+        return matrix
+
+    for x in range(n):
+        row = []
+        for y in range(x + 1):
+            result = comb(x, y)
+            row.append(result)
+
+        matrix.append(row)
+
+    return matrix
