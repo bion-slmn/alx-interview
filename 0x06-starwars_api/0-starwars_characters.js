78e65e7e583486447c31b6d526cadb0f0aab3ccc
bion-slmn <bionsol25@gmail.com>
2024-02-15 12:14:52 -0800

    semi standard styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,38 @@
 #!/usr/bin/node
 
 const request = require('request');
-const args = require('process').argv[2]
+const args = require('process').argv[2];
 
 const url = `https://swapi-api.alx-tools.com/api/films/${args}`;
 
 request(url, function (error, response, body) {
- if (!error) 
-  {
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-  
-  (async () => {
-     for (const value of characters) {
-      try {
-const name = await fetchName(value);
-console.log(name);
-}
-catch (error) {
-   console.error(error);
-}
-}
-})();
- }});
+  if (!error) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
 
-
-async function fetchName(url_) {
-	return new Promise(function (resolve, reject) {
-	request(url_, function (error, response, body) {
-	if (response) {
-	  const person = JSON.parse(body)
-	  resolve(person.name)
-	  }
-	});
+    (async () => {
+      for (const value of characters) {
+        try {
+          const name = await fetchName(value);
+          console.log(name);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
+  }
 });
+
+async function fetchName (url_) {
+  return new Promise(function (resolve, reject) {
+    request(url_, function (error, response, body) {
+      if (response) {
+        const person = JSON.parse(body);
+        resolve(person.name);
+      }
+      if (error) {
+        reject(error);
+      }
+    });
+  });
 }
